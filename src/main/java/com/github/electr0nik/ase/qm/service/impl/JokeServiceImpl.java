6f19969db10cdf9f75297522ca5cc977772927d0
Nik <Zkorpio@freenet.de>
2015-06-26 10:02:41 +0200

    simple joke bugfix

--- src/main/java/com/github/electr0nik/ase/qm/service/impl/JokeServiceImpl.java
+++ src/main/java/com/github/electr0nik/ase/qm/service/impl/JokeServiceImpl.java
@@ -21,65 +21,65 @@ import java.util.List;
 @Service
 public class JokeServiceImpl implements JokeService {
 
-  private DozerBeanMapper mapper;
+    private DozerBeanMapper mapper;
 
-  private ProducerTemplate producerTemplate;
+    private ProducerTemplate producerTemplate;
 
-  @Autowired
-  public JokeServiceImpl(DozerBeanMapper mapper, ProducerTemplate producerTemplate) {
-    this.mapper = mapper;
-    this.producerTemplate = producerTemplate;
-  }
-
-  @Override
-  public List<JokeDomain> getSimpleRandomJoke(List<Integer> idList) {
-    String routeFrom = "direct:simpleJoke";
-    return this.getMappedJokeDomainResponse(routeFrom, idList, 1L, null, null);
-  }
-
-  @Override
-  public List<JokeDomain> getSpecificJoke(List<Integer> idList, JokeTO jokeModel) {
-    String routeFrom = "direct:start";
-
-    JokeDomain.Builder builder = new JokeDomain.Builder();
-    this.mapper.map(jokeModel, builder);
-    return this.getMappedJokeDomainResponse(routeFrom, idList, jokeModel.getAmount(), builder.create(), null);
-  }
-
-  private List<JokeDomain> getMappedJokeDomainResponse(
-      String from, List<Integer> idList, Long amount, JokeDomain jokeDomain, Integer iteration) {
-    List<JokeDomain> returnValue = new ArrayList<>();
-
-    JsonModel jsonModel = (JsonModel) this.producerTemplate.send(from, exchange -> {
-      if (jokeDomain != null) {
-        exchange.setProperty("params", jokeDomain);
-      }
-    }).getOut().getBody();
-
-    // iterate over list; but since there is only one entry
-    JokeDomain.Builder builder = new JokeDomain.Builder();
-    jsonModel.getValue().forEach(stringObjectMap -> {
-      int id = (int) stringObjectMap.get(JsonModelHashMapEnum.ID.getValue());
-      if (!idList.contains(id)) {
-        final boolean[] filter = {false};
-        if (!jokeDomain.getWordList().isEmpty()) {
-          jokeDomain.getWordList().forEach(badWord -> {
-            filter[0] = ((String) stringObjectMap.get(JsonModelHashMapEnum.JOKE.getValue())).contains(badWord);
-          });
-        }
-        builder.setId(id);
-        builder.setJoke((String) stringObjectMap.get(JsonModelHashMapEnum.JOKE.getValue()));
-        builder.setLimitToList((List<String>) stringObjectMap.get(JsonModelHashMapEnum.CATEGORIES.getValue()));
-
-        if (!filter[0]) {
-          returnValue.add(builder.create());
-        }
-      }
-    });
-
-    if (returnValue.size() < amount && (iteration == null || iteration < 1)) {
-      this.getMappedJokeDomainResponse(from, idList, amount, jokeDomain, 1);
+    @Autowired
+    public JokeServiceImpl(DozerBeanMapper mapper, ProducerTemplate producerTemplate) {
+        this.mapper = mapper;
+        this.producerTemplate = producerTemplate;
     }
-    return returnValue;
-  }
+
+    @Override
+    public List<JokeDomain> getSimpleRandomJoke(List<Integer> idList) {
+        String routeFrom = "direct:simpleJoke";
+        return this.getMappedJokeDomainResponse(routeFrom, idList, 1L, null, null);
+    }
+
+    @Override
+    public List<JokeDomain> getSpecificJoke(List<Integer> idList, JokeTO jokeModel) {
+        String routeFrom = "direct:start";
+
+        JokeDomain.Builder builder = new JokeDomain.Builder();
+        this.mapper.map(jokeModel, builder);
+        return this.getMappedJokeDomainResponse(routeFrom, idList, jokeModel.getAmount(), builder.create(), null);
+    }
+
+    private List<JokeDomain> getMappedJokeDomainResponse(
+            String from, List<Integer> idList, Long amount, JokeDomain jokeDomain, Integer iteration) {
+        List<JokeDomain> returnValue = new ArrayList<>();
+
+        JsonModel jsonModel = (JsonModel) this.producerTemplate.send(from, exchange -> {
+            if (jokeDomain != null) {
+                exchange.setProperty("params", jokeDomain);
+            }
+        }).getOut().getBody();
+
+        // iterate over list; but since there is only one entry
+        JokeDomain.Builder builder = new JokeDomain.Builder();
+        jsonModel.getValue().forEach(stringObjectMap -> {
+            int id = (int) stringObjectMap.get(JsonModelHashMapEnum.ID.getValue());
+            if (!idList.contains(id)) {
+                final boolean[] filter = {false};
+                if (jokeDomain != null && jokeDomain.getWordList() != null && !jokeDomain.getWordList().isEmpty()) {
+                    jokeDomain.getWordList().forEach(badWord -> {
+                        filter[0] = ((String) stringObjectMap.get(JsonModelHashMapEnum.JOKE.getValue())).contains(badWord);
+                    });
+                }
+                builder.setId(id);
+                builder.setJoke((String) stringObjectMap.get(JsonModelHashMapEnum.JOKE.getValue()));
+                builder.setLimitToList((List<String>) stringObjectMap.get(JsonModelHashMapEnum.CATEGORIES.getValue()));
+
+                if (!filter[0]) {
+                    returnValue.add(builder.create());
+                }
+            }
+        });
+
+        if (returnValue.size() < amount && (iteration == null || iteration < 1)) {
+            this.getMappedJokeDomainResponse(from, idList, amount, jokeDomain, 1);
+        }
+        return returnValue;
+    }
 }
